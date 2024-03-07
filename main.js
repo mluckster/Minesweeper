bc446b25516675812c7afb3decfe3256faeb339f
Michael Lucky <luckymichael00@gmail.com>
2024-03-06 23:45:51 -0500

    modify script, checking individual cells

--- main.js
+++ main.js
@@ -5,16 +5,9 @@ generate.addEventListener('click', generateBoard)
 
 // generates board for the squares to appear
 function generateBoard(e) {
-    if (e.target.id == 'easy') {
-        var randomNumbers = generateMines(10, 8, 8) // calls to randomly generate location of mines (2d array)
-        makeSquares(randomNumbers, 8, 8)
-    }
-    else if (e.target.id == 'intermediate') {
-        var randomNumbers = generateMines(40, 256)
-    }
-    else {
-        var randomNumbers = generateMines(99, 480)
-    }
+    var randomNumbers = generateMines(10, 8, 8) // calls to randomly generate location of mines (2d array)
+    makeSquares(randomNumbers, 8, 8)
+    board.addEventListener('click', checkMine)
 }
 
 // generates an array of numbers which will correspond to the location of the mines
@@ -26,7 +19,11 @@ function generateMines(numMine, row, col) {
             Math.floor(Math.random() * row), 
             Math.floor(Math.random() * col)
             ]
-        if(numbers.includes(randomNumber) == false) {
+        console.log(numbers, randomNumber)
+        const found = numbers.some((arr) => {
+            return arr[0] === randomNumber[0] && arr[1] === randomNumber[1]
+        })
+        if (!found){
             numbers.push(randomNumber)
         }
     }
@@ -39,17 +36,36 @@ function makeSquares(randomNumbers, row, col) {
             const square = document.createElement('div')
             board.appendChild(square)
             square.classList = 'square'
+            square.id = `${j}-${i}`
             square.innerHTML = `${j}, ${i}`
 
             const found = randomNumbers.some((arr) => {
                 return arr[0] === i && arr[1] === j
             })
-            console.log(randomNumbers.length)
 
             if (found){
-                console.log('mine')
                 square.classList.add('mine')
+                const mine = document.createElement('div')
+                square.appendChild(mine)
+                mine.classList = 'mine-css'
             }
         }
     }
+}
+
+function checkMine(e) {
+    console.log(e.target.classList)
+    if (e.target.classList.contains('mine')){
+        e.target.style.backgroundColor = 'red'
+    }
+    else {
+        e.target.style.backgroundColor = 'green'
+    }
+    const cell = [Number(e.target.id[0]), Number(e.target.id[2])]
+    scanSurroundings(cell)
+}
+
+function scanSurroundings(cell){
+    let minesAroundCell = 0
+    console.log(board.children)
 }
