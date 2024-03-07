071c7435ae1f89455c11bca68cf12b5493d948eb
Michael Lucky <luckymichael00@gmail.com>
2024-03-07 17:52:41 -0500

    uncovers adjacent squares if there are no surrounding mines

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const board = document.getElementById('board')
 const generate = document.getElementById('easy')
+let numberOfMines = 0
+let numberOfFlags = 0
 
 generate.addEventListener('click', generateBoard)
+
+// win condition is uncovering all the squares that are NOT mines
 
 // generates board for the squares to appear
 function generateBoard(e) {
     var randomNumbers = generateMines(10, 8, 8) // calls to randomly generate location of mines (2d array)
+    numberOfMines = 10 // 10 for easy difficulty - need to change after if I put in other diffs
     makeSquares(randomNumbers, 8, 8)
     board.addEventListener('click', checkMine)
 }
@@ -55,23 +60,51 @@ function makeSquares(randomNumbers, row, col) {
 function checkMine(e) {
     if (e.target.classList.contains('mine')){
         e.target.style.backgroundColor = 'red'
+        // GAME IF LOST IF THIS HAPPENS!!!!!
     }
     else {
-        e.target.style.backgroundColor = 'green'
+        e.target.classList.add('active')
         const cell = [Number(e.target.id[0]), Number(e.target.id[1])]
         const surrounding = scanSurroundings(cell)
-        console.log(surrounding)
+        if (surrounding == 0){
+            uncoverAdjacentSquares(e.target)
+        }
+
         e.target.innerHTML = `${surrounding}`
     }
 }
 
+function uncoverAdjacentSquares(square){
+    // we can do row  = square.id[0]
+    // col = square.id[1]
+    // then we can 2d loop through row -1 to row +1 and within col -1 to col + 1
+    // finding the element by using: document.getElementById(''${i}${j})
 
-// this is slightly broken right now //
+    let row = Number(square.id[0])
+    let col = Number(square.id[1])
+
+    for (let i = row - 1; i <= row + 1; i++) {
+        for (let j = col - 1; j <= col + 1; j++) {
+            const element = document.getElementById(`${i}${j}`)
+            if (element && (i != row || j != col)) { // cases around the edge of the board -- element would be null
+                const surroundingMines = scanSurroundings([i, j])
+                element.classList.add('active')
+                element.innerHTML = `${surroundingMines}`
+                console.log(square, element)
+                if (surroundingMines == 0 && !element.classList.contains('visited')) {
+                    element.classList.add('visited')
+                    uncoverAdjacentSquares(element)
+                }
+            }
+        }
+    }
+}
+
+
+// if statements check each cell surrounding the one that was clicked to see if there's a mine
 function scanSurroundings(cell){
     let minesAroundCell = 0
-    console.log(minesAroundCell)
     Array.from(board.children).forEach(element => {
-        console.log(Number(element.id[0]) - 1 === cell[0], Number(element.id[0]), cell[0])
         if ( 
             Number(element.id[0]) === cell[0] - 1 &&
             (
@@ -105,6 +138,5 @@ function scanSurroundings(cell){
                 minesAroundCell += 1
             }
     });
-    console.log(minesAroundCell)
     return minesAroundCell
 }
